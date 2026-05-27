44793851e63fc1f22a6b151ac2c71808803a2cff
Osam Sami <osamsamiawan0465@gmail.com>
2026-05-27 09:05:52 +0500

    chore(android): bump minSdk to 21 to prevent camera native crashes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,11 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.dietlog.diet_log"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion  // <--- YAHAN STRICTLY 21 LIKHO
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
